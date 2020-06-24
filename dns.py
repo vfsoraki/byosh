451d536798cd56fe85a9c78ab2329b7de96a69e4
Vahid Fazlollahzade <vahid.fazl2000+gitlab@gmail.com>
2020-06-24 19:35:24 +0430

    Small improvements to dns.py

--- dns.py
+++ dns.py
@@ -5,6 +5,14 @@ import sys, socket, argparse
 from dnslib import DNSRecord, DNSHeader, RR, A, QTYPE
 from os import environ
 
+
+
+def print_debug_fn(debug):
+    if debug:
+        return lambda l : print(l)
+    else:
+        return lambda l : None
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process input')
     parser.add_argument("--ip", help="set listen ip address, set to ENV to get it from PUB_IP Env Variable", action="store", type=str, default="0.0.0.0")
@@ -13,11 +21,13 @@ if __name__ == '__main__':
     parser.add_argument("--debug", help="enable debug logging", action="store_true")
     args = parser.parse_args()
 
-    if args.debug:
-        print('IP: %s Port: %s' % (args.ip, args.port))
+    debug = print_debug_fn(args.debug)
+
+    debug('IP: %s Port: %s' % (args.ip, args.port))
 
     if str(args.ip).upper() == "ENV":
         args.ip = environ.get("PUB_IP")
+        debug('ENV IP: %s' % (args.ip,))
 
     udp_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_sock.bind(("0.0.0.0", args.port))
@@ -26,6 +36,7 @@ if __name__ == '__main__':
     w_list = []
     if environ.get("DNS_ALLOW_ALL") == "YES" or args.whitelist == "ALL":
         allow_all = True
+        debug('Allowing all IPs')
     else:
         if args.whitelist != "Empty":
             with open(args.whitelist) as f:
@@ -46,14 +57,11 @@ if __name__ == '__main__':
                             print(e)
                         realip = args.ip
                     r.add_answer(RR(qdom,rdata=A(realip),ttl=60))
-                    if args.debug:
-                        print("Request: %s --> %s" % (qdom.idna(), realip))
+                    debug("Request: %s --> %s" % (qdom.idna(), realip))
                 else:
                     r.add_answer(RR(qdom,rdata=A(args.ip),ttl=60))
-                    if args.debug:
-                        print("Request: %s --> %s" % (qdom.idna(), args.ip))
+                    debug("Request(local): %s --> %s" % (qdom.idna(), args.ip))
                 udp_sock.sendto(r.pack(), addr)
     except KeyboardInterrupt:
-        if args.debug:
-            print("done.")
+        debug("done.")
     udp_sock.close()
